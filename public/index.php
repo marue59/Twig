923762f23ef98a5206972ee17e75f16e2abd60c1
Marie Marie <Mariedefre87@gmail.com>
2019-10-22 10:35:53 +0200

    quetes Twig

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+require_once '../vendor/autoload.php';
+
+$products = ['product1', 'product2', 'product3', 'product4', 'product5'];
+
+$loader = new \Twig\Loader\FilesystemLoader( '../src/View');
+
+$twig = new Twig\Environment($loader);
+
+echo $twig->render('index.html.twig', array('products'=>$products));
